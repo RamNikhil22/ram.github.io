922a05309453965b1c8004bb6fdbcd935d3742ec
Ram Nikhil <ramnikhil786@gmail.com>
2021-06-18 21:41:32 +0530

    Add files via upload

---
+++ telugucorner.com/js/amarika_aata_script.js
@@ -0,0 +1,1 @@
+var kadhalikalu=new Array(Array(2,5),Array(1,3,6),Array(2,4,7),Array(3,8),Array(1,6,9),Array(2,5,7,10),Array(3,6,8,11),Array(4,7,12),Array(5,10),Array(6,9,11),Array(7,10,12),Array(8,11));
